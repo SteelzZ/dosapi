44a3eb266d29be6252f0f7003ab6ad867049eb76
Tomas Bartkus <to.bartkus@gmail.com>
2016-04-08 22:19:15 +0300

    Rename banckend to node

--- dosapi.go
+++ dosapi.go
@@ -11,11 +11,11 @@ import (
 //
 // Sample request:
 //
-// curl -X POST -d'{"dest":"1.1.1.1", "nodeName":"nodeX"}'  http://192.168.99.100:8082/api/v1/backends -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRvLmJhcnRrdXNAZ21haWwuY29tIiwiZXhwIjoxNDU5MDg5MjQ3fQ.GnyZ3I569AsVjS4f-yDpov-TdcTfLXhtvF5Y8F-2yYM" -v
+// curl -X POST -d'{"dest":"1.1.1.1", "nodeName":"nodeX"}'  http://192.168.99.100:8082/api/v1/nodes -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRvLmJhcnRrdXNAZ21haWwuY29tIiwiZXhwIjoxNDU5MDg5MjQ3fQ.GnyZ3I569AsVjS4f-yDpov-TdcTfLXhtvF5Y8F-2yYM" -v
 //
-// The path of the endpoint will be: /backends
+// The path of the endpoint will be: /nodes
 // @method POST
-// @path /backends
+// @path /nodes
 // @protected
 func RegisterBackend(user *registry.User, dest string, nodeName string) (result bool, err error) {
 
